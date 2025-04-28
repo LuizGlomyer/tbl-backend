ada2d6b7717dbbb6654ac14de17d848745c2560e
Luiz Glomyer <glomyerjunior@hotmail.com>
2025-04-27 22:55:25 -0400

    feat: method for truncating tables before seeding

--- api/src/db/seeds/seed.ts
+++ api/src/db/seeds/seed.ts
@@ -16,26 +16,54 @@ import {
   userMario,
   userSolidSnake,
 } from '../../../test/fixtures/user.fixtures';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { TABLE_USERS } from '../schema/tables/users';
+import { TABLE_PLATFORMS } from '../schema/tables/content/platforms';
+import { TABLE_GAMES } from '../schema/tables/content/games';
+import { TABLE_MEDIA } from '../schema/tables/content/media';
+
+async function truncateTables() {
+  const useTestDb = process.env.USE_TEST_DB;
+  const databaseName = useTestDb
+    ? process.env.TEST_DB_NAME!
+    : process.env.DB_NAME!;
+  const databaseUrl = useTestDb
+    ? process.env.TEST_DATABASE_URL!
+    : process.env.DATABASE_URL!;
+  console.log('--Database: ' + databaseName);
+
+  const db = drizzle(databaseUrl);
+  const tables = [TABLE_USERS, TABLE_GAMES, TABLE_PLATFORMS, TABLE_MEDIA];
+  for (const table of tables) {
+    console.log(`TRUNCATE ${table}`);
+    await db.execute(`TRUNCATE TABLE ${table} RESTART IDENTITY CASCADE`);
+  }
+}
 
 async function bootstrap() {
-  console.log('seeds.ts');
-  const app = await NestFactory.create(AppModule);
+  console.log('--Seeding start');
+  await truncateTables();
 
+  const app = await NestFactory.create(AppModule);
   const userService = app.get(UserService);
   const platformsService = app.get(PlatformsService);
   const gamesService = app.get(GamesService);
 
+  console.log('INSERT users');
   const mario = await userService.create(userMario);
   const solidSnake = await userService.create(userSolidSnake);
 
+  console.log('INSERT platforms');
   const ps4 = await platformsService.create(requestPlaystation4);
   const nintendoSwitch = await platformsService.create(requestNintendoSwitch);
 
+  console.log('INSERT games');
   requestDeathStranding.games.platformId = ps4.platforms.id;
   requestSmashBrosUltimate.games.platformId = nintendoSwitch.platforms.id;
   const deathStranding = await gamesService.create(requestDeathStranding);
   const smashUltimate = await gamesService.create(requestSmashBrosUltimate);
 
+  console.log('--Seeding end');
   await app.close();
 }
 
